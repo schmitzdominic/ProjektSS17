8b315c9949cc32828e12ef458d1cf494d55dc40e
Dominic Schmitz <schmitzdominic@googlemail.com>
2017-04-05 20:52:42 +0200

    Zahlen Logik Versuch 1

--- app/src/main/java/de/projektss17/bonpix/recognition/C_Rules.java
+++ app/src/main/java/de/projektss17/bonpix/recognition/C_Rules.java
@@ -1,5 +1,8 @@
 package de.projektss17.bonpix.recognition;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Domi on 05.04.2017.
  */
@@ -9,6 +12,40 @@ public class C_Rules {
     public String formater(String txt){
         txt = txt.replaceAll(" ", "");
 
-        return txt;
+        List<String> betraege = new ArrayList();
+
+        String dummy = "";
+
+        for(int i = 0; i < txt.length(); i++){
+            if(this.isDigit(txt.charAt(i))){
+                dummy += txt.charAt(i);
+            }
+            if(this.isSeparate(txt.charAt(i))){
+                dummy += txt.charAt(i);
+            }
+            if(this.isReturn(txt.charAt(i))){
+                betraege.add(dummy);
+            }
+        }
+
+        String rueck = "";
+
+        for(String x : betraege){
+            rueck += (x + "\n");
+        }
+
+        return rueck;
+    }
+
+    public boolean isDigit(char c){
+        return (c >= '0' && c <= '9');
+    }
+
+    public boolean isReturn(char c){
+        return (c == '\n');
+    }
+
+    public boolean isSeparate(char c){
+        return (c == ',' || c == '.');
     }
 }
