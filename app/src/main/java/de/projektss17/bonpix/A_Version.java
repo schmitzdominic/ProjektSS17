41996d598c353d02c258767d8bcd1b4e4b6a010d
Uli <uli.satzger@freenet.de>
2017-04-09 23:14:49 +0200

    i_activity_einstellungen_version Activity erstellt + Button + S Klasse verknüpft Reviewanmerkungen verbessert

--- app/src/main/java/de/projektss17/bonpix/A_Version.java
+++ app/src/main/java/de/projektss17/bonpix/A_Version.java
@@ -1,18 +1,15 @@
 package de.projektss17.bonpix;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class A_Version extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.box_a__version);
+        setContentView(R.layout.box_version_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
