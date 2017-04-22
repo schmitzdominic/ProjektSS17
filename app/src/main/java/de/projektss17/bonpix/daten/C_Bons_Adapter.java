066ed9ab37c41cbc0f5e964e01e65d570586e7bf
Marcus <marcus-hedrich@gmx.de>
2017-04-22 19:32:38 +0200

    - RecyclerView Tested (working)

--- app/src/main/java/de/projektss17/bonpix/daten/C_Bons_Adapter.java
+++ app/src/main/java/de/projektss17/bonpix/daten/C_Bons_Adapter.java
@@ -1,9 +1,14 @@
 package de.projektss17.bonpix.daten;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -21,11 +26,18 @@ public class C_Bons_Adapter extends RecyclerView.Adapter<C_Bons_Adapter.ViewHold
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView title, content;
+        public ImageView icon;
 
         public ViewHolder(View view){
             super(view);
+            icon = (ImageView) view.findViewById(R.id.imageview_picture);
             title = (TextView) view.findViewById(R.id.view_name);
             content = (TextView) view.findViewById(R.id.view_state);
+            Bitmap imageBitmap = BitmapFactory.decodeResource(view.getResources(),  R.drawable.icon_laden_aldi);
+            RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(view.getResources(), imageBitmap);
+            roundedBitmapDrawable.setCircular(true);
+            roundedBitmapDrawable.setAntiAlias(true);
+            icon.setImageDrawable(roundedBitmapDrawable);
         }
     }
 
@@ -46,6 +58,8 @@ public class C_Bons_Adapter extends RecyclerView.Adapter<C_Bons_Adapter.ViewHold
     @Override
     public void onBindViewHolder(ViewHolder holder, int position){
         C_Bons bon = bonsList.get(position);
+
+        //holder.icon.setImageDrawable(rounderBitmapDrawable);
         holder.title.setText(bon.getId());
         holder.content.setText(bon.getName());
     }
