16943f8c260166e1ac789d29fc3e9bc9f15378d5
Marcus <marcus-hedrich@gmx.de>
2017-04-24 21:19:50 +0200

    -

--- app/src/main/java/de/projektss17/bonpix/daten/C_Bons_Adapter.java
+++ app/src/main/java/de/projektss17/bonpix/daten/C_Bons_Adapter.java
@@ -35,7 +35,7 @@ public class C_Bons_Adapter extends RecyclerView.Adapter<C_Bons_Adapter.ViewHold
             content = (TextView) view.findViewById(R.id.view_state);
 
             // TODO: Derzeit ist das "REWE" Icon fest eingebunden in die RecyclerViewList. Dies muss geändert werden, sobald die RecyclerViewList dynamisch befüllt wird. (derzeit feste test werte, später Aldi, Lidl etc Logo je nach Bon)
-            Bitmap imageBitmap = BitmapFactory.decodeResource(view.getResources(),  R.drawable.icon_laden_rewe);
+            Bitmap imageBitmap = BitmapFactory.decodeResource(view.getResources(),  R.mipmap.icon_laden_rewe_24dp);
             RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(view.getResources(), imageBitmap);
             roundedBitmapDrawable.setCircular(true);
             roundedBitmapDrawable.setAntiAlias(true);
