f0b7edfdb65589c225eb26df88ab8375abef48b4
Marcus <marcus-hedrich@gmx.de>
2017-05-06 21:37:31 +0200

    update

--- app/src/main/java/de/projektss17/bonpix/A_Tab2Statistik.java
+++ app/src/main/java/de/projektss17/bonpix/A_Tab2Statistik.java
@@ -13,11 +13,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.github.mikephil.charting.data.BarEntry;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import de.projektss17.bonpix.daten.C_Statistik_Adapter;
 
 
