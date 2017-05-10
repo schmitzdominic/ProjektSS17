a19d8a74208e178386d0c96dcc4e6e05ab3982e1
Fabian <thysodeluxe92@googlemail.com>
2017-05-10 23:43:01 +0200

    I_garantie_liste_implementieren: - Erstellen Fragment für F_Laeden - A_Laeden mit F_Laeden verknüpft - Recycler View in F_Laeden erstellt - C_Laeden_Adapter erstellt und mit F_Laeden verknüft

--- app/src/main/java/de/projektss17/bonpix/A_Laeden.java
+++ app/src/main/java/de/projektss17/bonpix/A_Laeden.java
@@ -1,18 +1,36 @@
 package de.projektss17.bonpix;
 
+
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import de.projektss17.bonpix.fragments.F_Laeden;
+
 
 public class A_Laeden extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.box_laeden_screen);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new F_Laeden()).commit();
+
+       // FragmentManager fragmentManager = getFragmentManager();
+        //FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        //Fragment fLaeden = new F_Laeden();
+        //fragmentTransaction.add(R.id.laeden_container, fLaeden);
+        // fragmentTransaction.commit();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,6 +43,9 @@ public class A_Laeden extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
+
 
 }
