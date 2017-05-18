0c05c0551ea98fe0607e7524eb20c8d1e7d0a8e9
SemperF1 <alexanderleicher@gmx.de>
2017-05-18 16:49:46 +0200

    Floating Button verschwindet nun auch beim Scrollen

--- app/src/main/java/de/projektss17/bonpix/A_Gruppen.java
+++ app/src/main/java/de/projektss17/bonpix/A_Gruppen.java
@@ -23,6 +23,7 @@ public class A_Gruppen extends AppCompatActivity {
     private List<C_Gruppe> gruppenListe = new ArrayList<>();
     private RecyclerView recyclerViewGruppen;
     private C_Adapter_Gruppe mAdapter;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +32,8 @@ public class A_Gruppen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.gruppen_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        this.fab = (FloatingActionButton) findViewById(R.id.gruppen_fab);
+        this.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -51,6 +52,27 @@ public class A_Gruppen extends AppCompatActivity {
                 new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
         recyclerViewGruppen.setItemAnimator(new DefaultItemAnimator());
         recyclerViewGruppen.setAdapter(mAdapter);
+
+        recyclerViewGruppen.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx,int dy){
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (dy >0) {
+                    // Scroll Down
+                    if (fab.isShown()) {
+                        fab.hide();
+
+                    }
+                }
+                else if (dy <0) {
+                    // Scroll Up
+                    if (!fab.isShown()) {
+                        fab.show();
+                    }
+                }
+            }
+        });
 
     }
 
