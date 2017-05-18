dfe30922ab1660fcdd6c80b1d901948bd56b4400
SemperF1 <alexanderleicher@gmx.de>
2017-05-18 16:35:52 +0200

    Recycler View für Gruppen erstellt. Wird mit Testdaten befüllt. Löschenfunktion eingefügt.

--- app/src/main/java/de/projektss17/bonpix/A_Gruppen.java
+++ app/src/main/java/de/projektss17/bonpix/A_Gruppen.java
@@ -4,10 +4,25 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import de.projektss17.bonpix.daten.C_Gruppe;
+import de.projektss17.bonpix.daten.C_Adapter_Gruppe;
+
 public class A_Gruppen extends AppCompatActivity {
+
+    private List<C_Gruppe> gruppenListe = new ArrayList<>();
+    private RecyclerView recyclerViewGruppen;
+    private C_Adapter_Gruppe mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +40,31 @@ public class A_Gruppen extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        this.recyclerViewGruppen = (RecyclerView) findViewById(R.id.recyclerview_gruppen);
+
+        mAdapter = new C_Adapter_Gruppe(gruppenListe);
+        prepareGruppenData();
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerViewGruppen.setLayoutManager(mLayoutManager);
+        recyclerViewGruppen.addItemDecoration(
+                new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerViewGruppen.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewGruppen.setAdapter(mAdapter);
+
     }
 
+    private void prepareGruppenData(){
+// ToDo: Wenn die Benötigten Funktionen Bestehen werden hier die Daten aus der Datenbank ausgelesen und zurückgegeben (Ggf. noch anpassen da die Namen noch nicht bekannt sind)
+
+        gruppenListe.clear();
+
+        for(int i = 0; i < 10; i++){
+            gruppenListe.add(new C_Gruppe("TESTGRUPPE"+(i+1)));
+            Log.e("### GRUPPE", "TESTGRUPPE" + (i+1) + " wurde erstellt");
+        }
+
+        mAdapter.notifyDataSetChanged();
+    }
 
 }
-
