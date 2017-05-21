b033c1b646b3dff98551bea8c5c33044c52618e0
SshaBrn <Saschabraun83@gmail.com>
2017-05-21 19:49:34 +0200

    Änderung des Layouts :
    - Layout verändert  zuerst eine Recyclerview dann 2 CardViews nebeneinander
    - eine LineChart hinzugefügt (Änderungen an den Einstellungen der Line Chart vorgenommen)
    - eine Budget Card hinzugefügt (ToDo Budget aus der Datenbank holen)
    - Überschriften Layout verändert

--- app/src/main/java/de/projektss17/bonpix/A_Tab1Home.java
+++ app/src/main/java/de/projektss17/bonpix/A_Tab1Home.java
@@ -30,18 +30,15 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import de.projektss17.bonpix.daten.C_Bon;
-import de.projektss17.bonpix.daten.C_Bons_Adapter;
 import de.projektss17.bonpix.daten.C_Home_Adapter;
 
 public class A_Tab1Home extends Fragment {
 
-    private RecyclerView recyclerView;
-    private C_Home_Adapter mAdapter;
+
     private List<C_Bon> bonsList = new ArrayList<>();
     private RecyclerView Recyclerview;
     private C_Home_Adapter mAdapter1;
-    private LayoutInflater inflater;
-    private ViewGroup container;
+
     private View rootView;
 
     LineChart chart;
@@ -71,11 +68,8 @@ public class A_Tab1Home extends Fragment {
         chart.animateXY(2000, 4000);
         chart.setPadding(30, 30, 30, 30);
 
+
         prepareLineData();
-
-        this.inflater = inflater;
-        this.container = container;
-
         return rootView;
     }
 
@@ -117,6 +111,7 @@ public class A_Tab1Home extends Fragment {
         }
     }
 
+    // TODO: 21.05.2017 aktuelles Budget aus der Datenbank holen
     private void prepareBudgetData(){
 
     }
@@ -135,142 +130,3 @@ public class A_Tab1Home extends Fragment {
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-    // ALTER CODE:
-
-
-/*    // RECYCLERVIEW VARIABLEN
-
-    private List<C_Bon> bonsList = new ArrayList<>();
-    private RecyclerView recyclerView;
-    private C_Bons_Adapter mAdapter;
-
-    // CHART VARIABLEN
-
-    private PieChart pieChart1, pieChart2;
-    private BarChart bChart, bChart3;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.box_tab1_home_content, container, false);
-        recyclerView = (RecyclerView) rootView.findViewById(R.id.view_home_bons);
-        mAdapter = new C_Bons_Adapter(bonsList);
-        prepareHomeData();
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(container.getContext());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.addItemDecoration(
-                new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(mAdapter);
-        mAdapter.notifyDataSetChanged();
-
-        // Unterstreichen der Textviews
-
-        TextView textView = (TextView) rootView.findViewById(R.id.home_title1);
-        SpannableString content = new SpannableString("Ihre letzten Bons:");
-        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
-        textView.setText(content);
-
-        TextView textView1 = (TextView) rootView.findViewById(R.id.home_title2);
-        SpannableString content1 = new SpannableString("Ihre Statistik:");
-        content1.setSpan(new UnderlineSpan(), 0, content1.length(), 0);
-        textView1.setText(content1);
-
-        // Kuchendiagramm
-        pieChart1 = (PieChart) rootView.findViewById(R.id.chart1);
-        createPieChart(pieChart1, "Test");
-/*
-        *//**//*//**//*//*Balkendiagramm 1
-        bChart = (BarChart) rootView.findViewById(R.id.chart2);
-        ArrayList<IBarDataSet> dataSets1 = new ArrayList<>();
-        createBarChart(bChart, dataSets1, "The year 2017");
-
-        //Balkendiagramm 2
-        bChart3 = (BarChart) rootView.findViewById(R.id.chart3);
-        ArrayList<IBarDataSet> dataSets3 = new ArrayList<>();
-        createBarChart(bChart3, dataSets3, "The year 2018");*//**//*
-
-        return rootView;
-    }
-
-
-    private void prepareHomeData(){
-        bonsList.clear();
-
-        ArrayList<C_Bon> bonList = S.dbHandler.getAllBons(S.db);
-
-        for(int i = bonList.size()-1; i >= bonList.size()-3; i--){
-            this.bonsList.add(bonList.get(i));
-        }
-        mAdapter.notifyDataSetChanged();
-    }
-
-    *//**//**
-     * Erstellt ein Balkendiagramm
-     * Zuerst werden die Werte gefüllt und dann in das Diagramm eingelesen
-     *//**//*
-    private void createBarChart(BarChart bar, ArrayList<IBarDataSet> daten, String name){
-        ArrayList<BarEntry> val = new ArrayList<BarEntry>();
-        prepareBarData(val);   // Daten werden gefüllt
-
-        BarDataSet set = new BarDataSet(val, name);
-        set.setColors(ColorTemplate.MATERIAL_COLORS);
-
-        daten.add(set);
-        BarData data = new BarData(daten);
-
-        data.setValueTextSize(10f);
-        data.setBarWidth(0.9f);
-
-        bar.setTouchEnabled(false);
-        bar.setData(data);
-    }
-
-    *//**//**
-     * Füllt die Values eines Balkendiagramms mit random Werten
-     *//**//*
-    private void prepareBarData(ArrayList<BarEntry> values){
-        for (int i = 0; i < 10 + 1; i++) {
-            float val = (float) (Math.random());
-            values.add(new BarEntry(i, val));
-        }
-    }
-
-    *//**//**
-     * Erstellt ein Kreisdiagramm
-     * Zuerst werden die Werte gefüllt und dann in das Diagramm eingelesen
-     *//**//*
-    private void createPieChart(PieChart bar, String name){
-        ArrayList<PieEntry> val = new ArrayList<>();
-        preparePieData(val);    // Daten werden gefüllt
-
-        final PieDataSet dataSet = new PieDataSet(val, name);
-        final PieData pieData = new PieData(dataSet);
-
-        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
-        bar.setCenterText("Pie Chart");
-        bar.setData(pieData);
-        bar.animateY(1000);
-        bar.setBackgroundColor(5);
-        pieData.setDrawValues(true);
-    }
-
-    *//**//**
-     * Füllt die Values eines Kreisdiagramms mit Werten
-     *//**//*
-    private void preparePieData(ArrayList<PieEntry> values){
-        values.add(new PieEntry(0.2f, 0));
-        values.add(new PieEntry(0.2f, 1));
-        values.add(new PieEntry(0.50f, 2));
-    }
-}*/
