964de3e9e6f634ef49d4a43bb7bf4f1ddbe4f224
joeymez <joeymez1993@yahoo.de>
2017-05-27 12:14:31 +0200

    Update des items i_home_überarbeiten:
    - Erste Logikänderungen (Implementierung und Aufbau der RecyclerView)

--- app/src/main/java/de/projektss17/bonpix/A_Tab1Home.java
+++ app/src/main/java/de/projektss17/bonpix/A_Tab1Home.java
@@ -5,118 +5,35 @@ package de.projektss17.bonpix;
  * Hier bitte die Logik des ersten Tabs
  */
 
-import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-
 import android.support.v7.widget.DefaultItemAnimator;
-
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-
-import de.projektss17.bonpix.daten.C_Bon;
 import de.projektss17.bonpix.daten.C_Home_Adapter;
 
 public class A_Tab1Home extends Fragment {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private C_Home_Adapter homeAdapter;
 
-    private List<C_Bon> bonsList = new ArrayList<>();
-    private RecyclerView Recyclerview;
-    private C_Home_Adapter mAdapter1;
-
-    private View rootView;
-
-    LineChart chart;
-    LineDataSet dataSet;
-    ArrayList<ILineDataSet> lineDataSet;
-    LineData lineData;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.box_tab1_home_content, container, false);
-        Recyclerview = (RecyclerView) rootView.findViewById(R.id.tab_eins_recyclerview_bons);
-        mAdapter1 = new C_Home_Adapter(bonsList);
-        prepareHomeData();
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(container.getContext());
-        Recyclerview.setLayoutManager(mLayoutManager);
-        Recyclerview.addItemDecoration(
-                new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
-        Recyclerview.setItemAnimator(new DefaultItemAnimator());
-        Recyclerview.setAdapter(mAdapter1);
-        mAdapter1.notifyDataSetChanged();
-        chart = (LineChart) rootView.findViewById(R.id.chart);
-
-        // Chart Einstellungen:
-
-        chart.animateXY(2000, 4000);
-        chart.setPadding(30, 30, 30, 30);
+        View rootView = inflater.inflate(R.layout.box_tab1_home_content, container, false);
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.tab_home_recycler_view);
+        homeAdapter = new C_Home_Adapter();
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(homeAdapter);
+        homeAdapter.notifyDataSetChanged();
 
 
-        prepareLineData();
         return rootView;
-    }
-
-    private void prepareLineData() {
-        this.lineDataSet = new ArrayList<>();
-
-        // Liste wird mit Daten aus der Datenbank befüllt
-
-        this.dataSet = new LineDataSet(S.dbHandler.getLineData(S.db, 4), "Bon");
-        dataSet.setColor(Color.BLACK); // Linienfarbe
-        dataSet.setCircleColor(Color.BLACK); // Punktfarbe
-        dataSet.setCircleSize(5); // Punktgröße
-        dataSet.setLineWidth(3f); // Dicke der Linien
-        XAxis xAxis = chart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setDrawAxisLine(true);
-        xAxis.setDrawGridLines(false);
-        xAxis.setAxisMaximum(3);
-        xAxis.setAxisMinimum(0);
-        xAxis.setLabelCount(3);
-        chart.getAxisRight().setEnabled(false); // no right axis
-        this.lineDataSet.add(this.dataSet);
-        this.lineData = new LineData(this.lineDataSet);
-
-
-        this.lineData.setValueTextSize(10f);
-
-        if (chart != null) {
-            this.chart.setTouchEnabled(false);
-            this.chart.setData(this.lineData);
-            this.chart.invalidate();
-
-        }
-    }
-
-    // TODO: 21.05.2017 aktuelles Budget aus der Datenbank holen
-    private void prepareBudgetData(){
-
-    }
-
-    private void prepareHomeData() {
-        bonsList.clear();
-        int count = 0;
-        for (C_Bon bon : S.dbHandler.getAllBons(S.db)) {
-            if (count == 3) {
-                break;
-            }
-            bonsList.add(bon);
-            count++;
-        }
-
     }
 
 }
--- app/src/main/java/de/projektss17/bonpix/daten/C_Home_Adapter.java
+++ app/src/main/java/de/projektss17/bonpix/daten/C_Home_Adapter.java
@@ -1,114 +1,190 @@
 package de.projektss17.bonpix.daten;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import java.util.List;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import java.util.ArrayList;
 
 import de.projektss17.bonpix.R;
 import de.projektss17.bonpix.S;
 
-public class C_Home_Adapter extends RecyclerView.Adapter<C_Home_Adapter.ViewHolder> {
+public class C_Home_Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private int count = 3;
+    private ArrayList<C_Bon> bons;
+    private ArrayList<C_Budget> budgets;
 
 
-    private List<C_Bon> bonsList;
-    private int row_index = -1;
+    // LAYOUT BonCard
+    // (Hinweis: Drei feste Inhalte! Dynamisch würde kein Sinn machen, da hierfür die Tab Bons bereit steht! )
+    public class ViewHolderBonCard extends RecyclerView.ViewHolder {
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView title, content;
-        public ImageView icon;
-        public ImageView button;
+        public TextView firstContentAbove, firstContentBelow, secondContentAbove, secondContentBelow,
+                thirdContentAbove, thirdContentBelow;
+        public ImageView firstBonImage, firstFavoriteImage, secondBonImage, secondFavoriteImage,
+                thirdBonImage, thirdFavoriteImage;
 
-        public ViewHolder(View view){
+        public ViewHolderBonCard(View view) {
             super(view);
-            icon = (ImageView) view.findViewById(R.id.tab_home_boncard_first_bon_small_image);
-            title = (TextView) view.findViewById(R.id.tab_home_boncard_first_bon_above_content);
-            content = (TextView) view.findViewById(R.id.tab_home_boncard_first_bon_below_content);
-            button = (ImageView) view.findViewById(R.id.tab_home_boncard_first_bon_big_image);
 
-            // TODO: Derzeit ist das "REWE" Icon fest eingebunden in die RecyclerViewList. Dies muss geändert werden, sobald die RecyclerViewList dynamisch befüllt wird. (derzeit feste test werte, später Aldi, Lidl etc Logo je nach Bon)
-            Bitmap imageBitmap = BitmapFactory.decodeResource(view.getResources(),  R.mipmap.ic_aldisuedlogo);
-            RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(view.getResources(), imageBitmap);
-            roundedBitmapDrawable.setCircular(true);
-            roundedBitmapDrawable.setAntiAlias(true);
-            icon.setImageDrawable(roundedBitmapDrawable);
-            // --- END TO DO ---
+            // Implementierung des Layouts der einzelnen Objekte für die CardView
+            this.firstContentAbove = (TextView)view.findViewById(R.id.tab_home_boncard_first_bon_above_content);
+            this.firstContentBelow = (TextView)view.findViewById(R.id.tab_home_boncard_first_bon_below_content);
+            this.secondContentAbove = (TextView)view.findViewById(R.id.tab_home_boncard_second_bon_above_content);
+            this.secondContentBelow = (TextView)view.findViewById(R.id.tab_home_boncard_second_bon_below_content);
+            this.thirdContentAbove = (TextView)view.findViewById(R.id.tab_home_boncard_third_bon_above_content);
+            this.thirdContentBelow = (TextView)view.findViewById(R.id.tab_home_boncard_third_bon_below_content);
+
+            this.firstBonImage = (ImageView)view.findViewById(R.id.tab_home_boncard_first_bon_big_image);
+            this.firstFavoriteImage = (ImageView)view.findViewById(R.id.tab_home_boncard_first_bon_small_image);
+            this.secondBonImage = (ImageView)view.findViewById(R.id.tab_home_boncard_second_bon_big_image);
+            this.secondFavoriteImage = (ImageView)view.findViewById(R.id.tab_home_boncard_second_bon_small_image);
+            this.thirdBonImage = (ImageView)view.findViewById(R.id.tab_home_boncard_third_bon_big_image);
+            this.thirdFavoriteImage = (ImageView)view.findViewById(R.id.tab_home_boncard_third_bon_small_image);
+
         }
     }
 
-    /**
-     * Constructor
-     * @param bonsList
-     */
-    public C_Home_Adapter(List<C_Bon> bonsList){
-        this.bonsList = bonsList;
-    }
 
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-        Log.e("### BONS", "ONCREATEVIEWHOLDER");
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.box_home_boncard_layout, parent, false);
-        return new ViewHolder(itemView);
-    }
+    // LAYOUT BudgetCard
+    public class ViewHolderBudgetCard extends RecyclerView.ViewHolder {
 
-    @Override
-    public void onBindViewHolder(final ViewHolder holder, final int position){
-        Log.e("### BONS", "ONBINDVIEWHOLDER");
-        final C_Bon bon = bonsList.get(position);
-        holder.title.setText(bon.getShopName());
-        holder.content.setText(bon.getDate());
-        //Loading the FavoriteList
-        if (bon.getFavourite()){
-            holder.button.setImageDrawable(holder.button.getContext().getResources().getDrawable(R.drawable.star));
-            holder.button.setColorFilter(holder.button.getContext().getResources().getColor(R.color.colorPrimary));
-        } else {
-            holder.button.setImageDrawable(holder.button.getContext().getResources().getDrawable(R.drawable.star_outline));
-            holder.button.setColorFilter(holder.button.getContext().getResources().getColor(R.color.colorPrimary));
+        public TextView budgetCurrently, yearBefore,monthBefore, yearAfter, monthAfter, progressPercentage, tagVon, tagBis;
+        public ProgressBar progressBar;
+
+        public ViewHolderBudgetCard(View view) {
+            super(view);
+
+            // Implementierung des Layouts der einzelnen Objekte in der View
+            this.budgetCurrently = (TextView) view.findViewById(R.id.budget_content);
+            this.monthBefore = (TextView) view.findViewById(R.id.budget_monat_von);
+            this.yearBefore = (TextView) view.findViewById(R.id.budget_jahr_von);
+            this.monthAfter = (TextView) view.findViewById(R.id.budget_monat_bis);
+            this.yearAfter = (TextView) view.findViewById(R.id.budget_jahr_bis);
+            this.tagVon = (TextView) view.findViewById(R.id.budget_tag_von);
+            this.tagBis = (TextView) view.findViewById(R.id.budget_tag_bis);
+            this.progressBar = (ProgressBar)view.findViewById(R.id.budget_progress_bar_circle);
+            this.progressPercentage = (TextView) view.findViewById(R.id.budget_progress_percentage);
+
         }
-        holder.button.setOnClickListener(new View.OnClickListener(){
+    }
 
-            /**
-             * OnClickListener for the RecyclerView
-             * @param v
-             */
-            @Override
-            public void onClick(View v) {
-                row_index = position;
-                // Put the onClick cases here
-                switch (v.getId()) {
-                    case R.id.tab_home_boncard_first_bon_big_image: {
-                        if (bon.getFavourite()){
-                            holder.button.setImageDrawable(v.getContext().getResources().getDrawable(R.drawable.star_outline));
-                            holder.button.setColorFilter(v.getContext().getResources().getColor(R.color.colorPrimary));
-                            bon.setFavourite(false);
-                            S.dbHandler.updateBon(S.db, bon);
-                        } else {
-                            if (row_index == position) {
-                                holder.button.setImageDrawable(v.getContext().getResources().getDrawable(R.drawable.star));
-                                holder.button.setColorFilter(v.getContext().getResources().getColor(R.color.colorPrimary));
-                                bon.setFavourite(true);
-                                S.dbHandler.updateBon(S.db, bon);
-                            }
-                        }
-                    }
-                    break;
-                }
+
+    // LAYOUT LineChartCard
+    public class ViewHolderLinechartCard extends RecyclerView.ViewHolder{
+
+        public LineChart lineChart;
+
+        public ViewHolderLinechartCard(View view){
+            super(view);
+
+            //Diese Logik war bereits gegeben vom vorherigen Sprint! (Bei Fragen an den jeweiligen wenden!)
+            //ToDo - Es muss noch entschieden weren, welche Daten ausgelesen und ausgewertet werden
+            LineDataSet dataSet;
+            ArrayList<ILineDataSet> lineDataSet;
+            LineData lineData;
+
+            this.lineChart = (LineChart)view.findViewById(R.id.tab_home_chartcard_linechart);
+            lineChart.animateXY(2000, 4000);
+            lineChart.setPadding(30, 30, 30, 30);
+
+            lineDataSet = new ArrayList<>();
+
+            dataSet = new LineDataSet(S.dbHandler.getLineData(S.db, 4), "Bon");
+            dataSet.setColor(Color.BLACK); // Linienfarbe
+            dataSet.setCircleColor(Color.BLACK); // Punktfarbe
+            dataSet.setCircleSize(5); // Punktgröße
+            dataSet.setLineWidth(3f); // Dicke der Linien
+            XAxis xAxis = lineChart.getXAxis();
+            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+            xAxis.setDrawAxisLine(true);
+            xAxis.setDrawGridLines(false);
+            xAxis.setAxisMaximum(3);
+            xAxis.setAxisMinimum(0);
+            xAxis.setLabelCount(3);
+            lineChart.getAxisRight().setEnabled(false); // no right axis
+            lineDataSet.add(dataSet);
+            lineData = new LineData(lineDataSet);
+
+            lineData.setValueTextSize(10f);
+
+            if (lineChart != null) {
+                this.lineChart.setTouchEnabled(false);
+                this.lineChart.setData(lineData);
+                this.lineChart.invalidate();
+
             }
-        });
+        }
+
     }
 
+
     @Override
-    public int getItemCount(){
-        Log.e("### BONS", "GETITEMCOUNT");
-        return bonsList.size();
+    public int getItemViewType(int position) {
+        return position;
     }
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        bons = S.dbHandler.getAllBons(S.db);
+        budgets = S.dbHandler.getAllBudgets(S.db);
+
+        View itemView;
+
+        switch(viewType){
+            case 0:
+                itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.box_home_boncard_layout, parent, false);
+                return new ViewHolderBonCard(itemView);
+
+            case 1:
+                itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.box_home_topbudget_layout, parent, false);
+                return new ViewHolderBudgetCard(itemView);
+            case 2:
+                itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.box_home_linechart_layout, parent, false);
+                return new ViewHolderLinechartCard(itemView);
+        }
+        return null;
+    }
+
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+
+        switch (getItemViewType(position)){
+            case 0:
+                ViewHolderBonCard holderBonCard = (ViewHolderBonCard) holder;
+                holderBonCard.firstContentAbove.setText("Test");
+                break;
+            case 1:
+                ViewHolderBudgetCard holderBudgetCard = (ViewHolderBudgetCard) holder;
+                holderBudgetCard.budgetCurrently.setText("TEST");
+                break;
+            case 2:
+                ViewHolderLinechartCard holderLinechartCard = (ViewHolderLinechartCard) holder;
+                holderLinechartCard.lineChart.setBackgroundColor(555885);
+                break;
+        }
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return count;
+    }
+
 }
