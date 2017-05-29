11cb36881a1896848115df57ad2835883cc70389
Dominic Schmitz <schmitzdominic@googlemail.com>
2017-05-29 19:00:21 +0200

    BUGFIX

--- app/src/main/java/de/projektss17/bonpix/A_Tab1Home.java
+++ app/src/main/java/de/projektss17/bonpix/A_Tab1Home.java
@@ -5,6 +5,7 @@ package de.projektss17.bonpix;
  * Hier bitte die Logik des ersten Tabs
  */
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -22,11 +23,13 @@ public class A_Tab1Home extends Fragment {
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private C_Home_Adapter homeAdapter;
+    public FloatingActionButton fabPlus;
     private View rootView;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         this.rootView = inflater.inflate(R.layout.box_tab1_home_content, container, false);
+        this.fabPlus = ((A_Main) getActivity()).getFloatingActionButtonPlus();
 
         return rootView;
     }
@@ -35,9 +38,33 @@ public class A_Tab1Home extends Fragment {
     public void onResume() {
         super.onResume();
 
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.tab_home_recycler_view);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx,int dy){
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (dy >0) {
+                    // Scroll Down
+                    if (fabPlus.isShown()) {
+
+                        if (((A_Main) getActivity()).getFabState()) {
+                            ((A_Main) getActivity()).closeFABMenu();
+                        }
+                        fabPlus.hide();
+                    }
+                }
+                else if (dy <0) {
+                    // Scroll Up
+                    if (!fabPlus.isShown()) {
+                        fabPlus.show();
+                    }
+                }
+            }
+        });
+
         if(S.dbHandler.getAllBons(S.db).size() > 2 && S.dbHandler.getAllBudgets(S.db).size() > 0) {
 
-            recyclerView = (RecyclerView) rootView.findViewById(R.id.tab_home_recycler_view);
             homeAdapter = new C_Home_Adapter(getActivity());
             layoutManager = new LinearLayoutManager(getActivity());
             recyclerView.setLayoutManager(layoutManager);
