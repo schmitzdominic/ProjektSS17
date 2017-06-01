5ce288713cfa95639c8d4db8b7d8e7c744f74269
joeymez <joeymez1993@yahoo.de>
2017-06-01 15:40:23 +0200

    Update des items i_home_finalisieren:
    - Hinzufügen einer Dynamischen Liste der Bons in der CardView
    - Hinzufügen neuer Funktionen zum Verarbeiten der Bons

--- app/src/main/java/de/projektss17/bonpix/daten/C_Home_Adapter.java
+++ app/src/main/java/de/projektss17/bonpix/daten/C_Home_Adapter.java
@@ -1,6 +1,7 @@
 package de.projektss17.bonpix.daten;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -18,6 +20,7 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import java.util.ArrayList;
 
+import de.projektss17.bonpix.A_Bon_Anzeigen;
 import de.projektss17.bonpix.R;
 import de.projektss17.bonpix.S;
 
@@ -41,48 +44,12 @@ public class C_Home_Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
     //  da hierfür die Tab Bons bereit steht und die implementierung möglicherweise nicht funktioniert! )
     public class ViewHolderBonCard extends RecyclerView.ViewHolder {
 
-        public Button bon1, bon2, bon3;
-
-        public TextView firstContentAbove, firstContentBelow, firstBonPrice,
-                secondContentAbove, secondContentBelow, secondBonPrice,
-                thirdContentAbove, thirdContentBelow, thirdBonPrice;
-        public ImageView firstBonImage, firstFavoriteImage, secondBonImage, secondFavoriteImage,
-                thirdBonImage, thirdFavoriteImage;
+        LinearLayout linearLayout;
 
         public ViewHolderBonCard(View view) {
             super(view);
 
-            // Implementierung des Layouts der einzelnen Objekte für die CardView
-            this.firstContentAbove = (TextView) view.findViewById(R.id.tab_home_boncard_first_bon_above_content);
-            this.firstContentBelow = (TextView) view.findViewById(R.id.tab_home_boncard_first_bon_below_content);
-            this.firstBonPrice = (TextView) view.findViewById(R.id.tab_home_boncard_first_bon_betrag);
-            this.secondContentAbove = (TextView) view.findViewById(R.id.tab_home_boncard_second_bon_above_content);
-            this.secondContentBelow = (TextView) view.findViewById(R.id.tab_home_boncard_second_bon_below_content);
-            this.secondBonPrice = (TextView) view.findViewById(R.id.tab_home_boncard_second_bon_betrag);
-            this.thirdContentAbove = (TextView) view.findViewById(R.id.tab_home_boncard_third_bon_above_content);
-            this.thirdContentBelow = (TextView) view.findViewById(R.id.tab_home_boncard_third_bon_below_content);
-            this.thirdBonPrice = (TextView) view.findViewById(R.id.tab_home_boncard_third_bon_betrag);
-
-
-            //ToDo - BonImages Rund machen (sind derzeit viereckig)
-            this.firstBonImage = (ImageView) view.findViewById(R.id.tab_home_boncard_first_bon_small_image);
-            this.firstFavoriteImage = proofFavorite(bons.get(0), (ImageView) view.findViewById(R.id.tab_home_boncard_first_bon_big_image));
-            this.secondBonImage = (ImageView) view.findViewById(R.id.tab_home_boncard_second_bon_small_image);
-            this.secondFavoriteImage = proofFavorite(bons.get(1), (ImageView) view.findViewById(R.id.tab_home_boncard_second_bon_big_image));
-            this.thirdBonImage = (ImageView) view.findViewById(R.id.tab_home_boncard_third_bon_small_image);
-            this.thirdFavoriteImage = proofFavorite(bons.get(2), (ImageView) view.findViewById(R.id.tab_home_boncard_third_bon_big_image));
-
-            //Implementierung der Invisible-Buttons zum Auswählen der zuletzt eingescannten Bons
-            this.bon1 = (Button) view.findViewById(R.id.tab_home_boncard_first_bon);
-            this.bon2 = (Button) view.findViewById(R.id.tab_home_boncard_second_bon);
-            this.bon3 = (Button) view.findViewById(R.id.tab_home_boncard_third_bon);
-
-
-            //ToDo - Hier muss ein onClick-Listener für die oberen drei Buttons (bon1, ...) implementiert werden
-            // ---->> onClick bewirkt eine startActivity (Intent) an die Activity Bon_Anzeigen
-            // HINWEIS: je nachdem welchen Bon man anklickt, sollen diese Daten an die Bon_Anzeigen weitergeleitet und autom. befüllt werden
-
-            // INFO: Implementierung im nächsten Sprint, da parallel zu diesem Item Bon_Anzeigen überarbeitet wurde!!!!
+            linearLayout = (LinearLayout)view.findViewById(R.id.tab_home_boncard_linearlayout);
 
         }
     }
@@ -197,47 +164,11 @@ public class C_Home_Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
         //HINWEIS: Derzeit sind alle drei Cards statisch. Die Inhalte sind jedoch DYNAMISCH!
         switch (getItemViewType(position)) {
             case 0:
+
                 final ViewHolderBonCard holderBonCard = (ViewHolderBonCard) holder;
-                holderBonCard.firstContentAbove.setText(bons.get(0).getShopName());
-                holderBonCard.firstContentBelow.setText(bons.get(0).getDate());
-                holderBonCard.firstBonPrice.setText(bons.get(0).getTotalPrice() + curreny);
-                holderBonCard.secondContentAbove.setText(bons.get(1).getShopName());
-                holderBonCard.secondContentBelow.setText(bons.get(1).getDate());
-                holderBonCard.secondBonPrice.setText(bons.get(1).getTotalPrice() + curreny);
-                holderBonCard.thirdContentAbove.setText(bons.get(2).getShopName());
-                holderBonCard.thirdContentBelow.setText(bons.get(2).getDate());
-                holderBonCard.thirdBonPrice.setText(bons.get(2).getTotalPrice() + curreny);
-                holderBonCard.firstBonImage.setImageResource(R.mipmap.ic_edekalogo);
 
-                holderBonCard.secondBonImage.setImageResource(R.mipmap.ic_edekalogo);
-                holderBonCard.thirdBonImage.setImageResource(R.mipmap.ic_edekalogo);
-
-
-                // Erster Favoriten-Button - Beim Drücken wird Funktion setAndChangeFavorite aufgerufen
-                holderBonCard.firstFavoriteImage.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        holderBonCard.firstFavoriteImage = setAndChangeFavorite(bons.get(0),holderBonCard.firstFavoriteImage);
-                    }
-                });
-
-
-                // Zweiter Favoriten-Button - Beim Drücken wird Funktion setAndChangeFavorite aufgerufen
-                holderBonCard.secondFavoriteImage.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        holderBonCard.secondFavoriteImage = setAndChangeFavorite(bons.get(1),holderBonCard.secondFavoriteImage);
-                    }
-                });
-
-
-                // Dritter Favoriten-Button - Beim Drücken wird Funktion setAndChangeFavorite aufgerufen
-                holderBonCard.thirdFavoriteImage.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        holderBonCard.thirdFavoriteImage = setAndChangeFavorite(bons.get(2),holderBonCard.thirdFavoriteImage);
-                    }
-                });
+                for(int i = 0; i < bons.size(); i++)
+                    holderBonCard.linearLayout.addView(inflateBonsRow(bons.get(i)),holderBonCard.linearLayout.getChildCount());
 
                 break;
             case 1:
@@ -312,30 +243,58 @@ public class C_Home_Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
         }
     }
 
-
     /**
-     * Wird beim onClick-Listener aufgerufen - Ändert die Farbe des ImageViews und setzt den Favoriten-Boolean entsprechend des Kriteriums
-     * INFO: Prüft ob true - wenn ja wird auf false gesetzt und die DB mit dem Bon geupdatet - Das selbe umgekehrt mit Anfangswert false !
-     * @param bon Übergabe eines ausgewählten Bons, welches überprüft wird
-     * @param favImage Übergabe der ImageView, die geändert wird ( In diesem Fall Favoriten-Image)
-     * @return Rückgabe des übergebenen, bereits veränderten, Images
+     * Erzeugt eine Dynamische Liste mit den aktuellsten Bons aus der DB
+     * @param bon Übergabe der einzelnen Bons
+     * @return Rückgabe der fertig gebauten View
      */
-    private ImageView setAndChangeFavorite(final C_Bon bon, final ImageView favImage) {
+    private View inflateBonsRow(final C_Bon bon){
 
-        if(bon.getFavourite()){
-            favImage.setImageDrawable(favImage.getContext().getResources().getDrawable(R.drawable.star_outline));
-            favImage.setColorFilter(favImage.getContext().getResources().getColor(R.color.colorPrimary));
-            bon.setFavourite(false);
-            S.dbHandler.updateBon(S.db, bon);
-            return favImage;
-        } else {
-            favImage.setImageDrawable(favImage.getContext().getResources().getDrawable(R.drawable.star));
-            favImage.setColorFilter(favImage.getContext().getResources().getColor(R.color.colorPrimary));
-            bon.setFavourite(true);
-            S.dbHandler.updateBon(S.db, bon);
-            return favImage;
-        }
+        final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View rowView = inflater.inflate(R.layout.box_bons_view_layout, null);
+        final TextView shopName = (TextView)rowView.findViewById(R.id.bons_shop_name);
+        final TextView date = (TextView)rowView.findViewById(R.id.bons_buying_date);
+        final TextView price = (TextView)rowView.findViewById(R.id.bons_price);
+        final ImageView shopIcon = (ImageView)rowView.findViewById(R.id.bons_shop_image);
+        final ImageView favIcon = proofFavorite(bon,(ImageView)rowView.findViewById(R.id.bons_favorite_icon));
+        //final Button wholeBon = (Button)rowView.findViewById(R.id.tab_home_boncard_bon);
 
+        shopName.setText(bon.getShopName());
+        date.setText(bon.getDate());
+        price.setText(bon.getTotalPrice() + " " + curreny);
+        shopIcon.setImageBitmap(S.getShopIcon(context.getResources(), bon.getShopName()));
+
+        // OnClickListener für den Favoriten Icon (setzt gleichzeitg true bzw. false beim drücken)
+        favIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (bon.getFavourite()) {
+                    favIcon.setImageDrawable(favIcon.getContext().getResources().getDrawable(R.drawable.star_outline));
+                    favIcon.setColorFilter(favIcon.getContext().getResources().getColor(R.color.colorPrimary));
+                    bon.setFavourite(false);
+                    S.dbHandler.updateBon(S.db, bon);
+                } else {
+                    favIcon.setImageDrawable(favIcon.getContext().getResources().getDrawable(R.drawable.star));
+                    favIcon.setColorFilter(favIcon.getContext().getResources().getColor(R.color.colorPrimary));
+                    bon.setFavourite(true);
+                    S.dbHandler.updateBon(S.db, bon);
+                }
+            }
+        });
+
+
+        // OnClickListener zum drücken der Bons (eigener unsichtbarer Button zur besseren Performance)
+        rowView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, A_Bon_Anzeigen.class);
+                intent.putExtra("BonPos", bon.getId());
+                context.startActivity(intent);
+            }
+        });
+
+        return rowView;
 
     }
 }
