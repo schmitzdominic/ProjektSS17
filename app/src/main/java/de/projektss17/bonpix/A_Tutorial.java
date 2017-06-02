e1725c9a351947c769a5f5e9daef79e7526cde43
Daniel <daniel@koenigl-bayern.de>
2017-06-02 15:47:40 +0200

    Slideshow erstellt
    - 4 Beispielimages eingefügt

--- app/src/main/java/de/projektss17/bonpix/A_Tutorial.java
+++ app/src/main/java/de/projektss17/bonpix/A_Tutorial.java
@@ -1,61 +1,25 @@
 package de.projektss17.bonpix;
 
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.os.Message;
-import android.widget.TextView;
 
+import de.projektss17.bonpix.daten.C_Adapter_Tutorial;
 
 public class A_Tutorial extends AppCompatActivity {
 
-    ImageView slide;
-    TextView msg;
+    ViewPager viewPager;
+    C_Adapter_Tutorial customSwip;
     Button btn;
-
-    int i=0;
-    int imgid[]={R.drawable.box_budget_circle_shape};
-
-    RefreshHandler refreshHandler=new RefreshHandler();
-
-    @SuppressLint("HandlerLeak")
-    class RefreshHandler extends Handler{
-        @Override
-        public void handleMessage(Message msg) {
-            // TODO Auto-generated method stub
-            A_Tutorial.this.updateUI();
-        }
-        public void sleep(long delayMillis){
-            this.removeMessages(0);
-            sendMessageDelayed(obtainMessage(0), delayMillis);
-        }
-    };
-    public void updateUI(){
-        int currentInt=Integer.parseInt((String)msg.getText())+10;
-        if(currentInt<=100){
-            refreshHandler.sleep(2000);
-            msg.setText(String.valueOf(currentInt));
-            if(i<imgid.length){
-                slide.setImageResource(imgid[i]);
-
-                // slide.setPadding(left, top, right, bottom);
-                i++;
-            }
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.box_tutorial_content);
-        slide = (ImageView) findViewById(R.id.slideshow);
-        msg = (TextView) findViewById(R.id.message);
+        viewPager=(ViewPager)findViewById(R.id.viewPager);
+        customSwip = new C_Adapter_Tutorial(this);
+        viewPager.setAdapter(customSwip);
 
         btn =(Button) findViewById(R.id.tutorial_skip_button);
 
@@ -65,7 +29,5 @@ public class A_Tutorial extends AppCompatActivity {
                 S.startActivitiy(A_Tutorial.this, A_Favoriten.class);
             }
         });
-
     }
-
 }
---
+++ app/src/main/java/de/projektss17/bonpix/daten/C_Adapter_Tutorial.java
@@ -0,0 +1,60 @@
+package de.projektss17.bonpix.daten;
+
+/**
+ * Created by Daniel on 02.06.2017.
+ */
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import de.projektss17.bonpix.R;
+
+
+public class C_Adapter_Tutorial extends PagerAdapter {
+    private int [] imageResources ={R.drawable.picture1,R.drawable.picture2,R.drawable.picture3,R.drawable.picture4};
+    private Context ctx;
+    private LayoutInflater layoutInflater;
+
+    public C_Adapter_Tutorial(Context c) {
+        ctx=c;
+
+    }
+
+    @Override
+    public int getCount() {
+        return imageResources.length;
+    }
+
+
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater= (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView=layoutInflater.inflate(R.layout.box_tutorial_adapter,container,false);
+        ImageView imageView=(ImageView) itemView.findViewById(R.id.swip_image_view);
+        TextView textView=(TextView) itemView.findViewById(R.id.imageCount);
+        imageView.setImageResource(imageResources[position]);
+        textView.setText("Tipp : "+(position+1));
+        container.addView(itemView);
+        return itemView;
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+
+        return  (view==object);
+    }
+}
+
