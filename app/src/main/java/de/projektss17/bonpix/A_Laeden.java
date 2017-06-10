a7821140c9718780465d66c48d7845056360b7c7
Dominic Schmitz <schmitzdominic@googlemail.com>
2017-06-10 23:06:41 +0200

    HOTFIX

--- app/src/main/java/de/projektss17/bonpix/A_Laeden.java
+++ app/src/main/java/de/projektss17/bonpix/A_Laeden.java
@@ -1,13 +1,9 @@
 package de.projektss17.bonpix;
 
-
-
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -15,7 +11,6 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
