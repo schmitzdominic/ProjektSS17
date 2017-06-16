93078b46422499f4175da3b52271605509a1d461
joeymez <joeymez1993@yahoo.de>
2017-06-16 19:40:44 +0200

    Finalisierug des Items i_main_tabs_tutorials

--- app/src/main/java/de/projektss17/bonpix/daten/C_Adapter_Tutorial.java
+++ app/src/main/java/de/projektss17/bonpix/daten/C_Adapter_Tutorial.java
@@ -1,9 +1,5 @@
 package de.projektss17.bonpix.daten;
 
-/**
- * Created by Daniel on 02.06.2017.
- */
-
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
@@ -22,9 +18,8 @@ import de.projektss17.bonpix.R;
 
 import static de.projektss17.bonpix.R.id.textView;
 
-
 public class C_Adapter_Tutorial extends PagerAdapter {
-    private int [] imageResources ={R.drawable.picture1,R.drawable.picture2,R.drawable.picture3,R.drawable.picture4,R.drawable.picture5,R.drawable.picture6,R.drawable.picture7,R.drawable.picture7, R.drawable.check};
+    private int[] imageResources = {R.drawable.picture1, R.drawable.picture2, R.drawable.picture3, R.drawable.picture4, R.drawable.picture5, R.drawable.picture6, R.drawable.picture7, R.drawable.picture7, R.drawable.check};
     private int progressCount = imageResources.length;
     private int slideCount;
     private Context ctx;
@@ -35,7 +30,7 @@ public class C_Adapter_Tutorial extends PagerAdapter {
     private View itemView;
 
     public C_Adapter_Tutorial(Context c, int slideCount) {
-        this.ctx=c;
+        this.ctx = c;
         this.slideCount = slideCount;
 
     }
@@ -46,64 +41,61 @@ public class C_Adapter_Tutorial extends PagerAdapter {
     }
 
 
-
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
 
-        layoutInflater= (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        this.itemView=layoutInflater.inflate(R.layout.box_tutorial_adapter,container,false);
+        layoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.itemView = layoutInflater.inflate(R.layout.box_tutorial_adapter, container, false);
 
-        if(position < imageResources.length - 1){
-            this.imageView=(ImageView) itemView.findViewById(R.id.swip_image_view);
-            this.content = (TextView) itemView.findViewById(R.id.tutorial_text_content);
-            this.progressBar = (ProgressBar) itemView.findViewById(R.id.tutorial_imageCount);
-            this.progressBarBackground = (ProgressBar)itemView.findViewById(R.id.tutorial_imageCountBackground);
+        this.imageView = (ImageView) itemView.findViewById(R.id.swip_image_view);
+        this.content = (TextView) itemView.findViewById(R.id.tutorial_text_content);
+        this.progressBar = (ProgressBar) itemView.findViewById(R.id.tutorial_imageCount);
+        this.progressBarBackground = (ProgressBar) itemView.findViewById(R.id.tutorial_imageCountBackground);
 
-            this.imageView.setImageResource(imageResources[position]);
-            this.progressBarBackground.setProgress(progressCount);
+        this.imageView.setImageResource(imageResources[position]);
+        this.progressBarBackground.setProgress(progressCount);
 
-            switch (position){
-                case 0:
-                    this.content.setText(ctx.getResources().getString((R.string.tutorial_text1)));
-                    this.progressBar.setProgress(position+1);   // Progress wird um 1 erhöht
-                    break;
-                case 1:
-                    this.content.setText(ctx.getResources().getString((R.string.tutorial_text2)));
-                    this.progressBar.setProgress(position+1);
-                    break;
-                case 2:
-                    this.content.setText(ctx.getResources().getString((R.string.tutorial_text3)));
-                    this.progressBar.setProgress(position+1);
-                    break;
-                case 3:
-                    this.content.setText(ctx.getResources().getString((R.string.tutorial_text4)));
-                    this.progressBar.setProgress(position+1);
-                    break;
-                case 4:
-                    this.content.setText(ctx.getResources().getString((R.string.tutorial_text5)));
-                    this.progressBar.setProgress(position+1);
-                    break;
-                case 5:
-                    this.content.setText(ctx.getResources().getString((R.string.tutorial_text6)));
-                    this.progressBar.setProgress(position+1);
-                    break;
-                case 6:
-                    this.content.setText(ctx.getResources().getString((R.string.tutorial_text7)));
-                    this.progressBar.setProgress(position+1);
-                    break;
-                case 7:
-                    this.imageView.setImageResource(R.drawable.check);
-                    this.content.setText(ctx.getResources().getString((R.string.tutorial_text8)));
-                    this.progressBar.setBackgroundColor(Color.WHITE);
-                    this.progressBarBackground.setBackgroundColor(Color.WHITE);
-            }
-
-
-            container.addView(itemView);
+        switch (position) {
+            case 0:
+                this.content.setText(ctx.getResources().getString((R.string.tutorial_text1)));
+                this.progressBar.setProgress(position + 1);   // Progress wird um 1 erhöht
+                break;
+            case 1:
+                this.content.setText(ctx.getResources().getString((R.string.tutorial_text2)));
+                this.progressBar.setProgress(position + 1);
+                break;
+            case 2:
+                this.content.setText(ctx.getResources().getString((R.string.tutorial_text3)));
+                this.progressBar.setProgress(position + 1);
+                break;
+            case 3:
+                this.content.setText(ctx.getResources().getString((R.string.tutorial_text4)));
+                this.progressBar.setProgress(position + 1);
+                break;
+            case 4:
+                this.content.setText(ctx.getResources().getString((R.string.tutorial_text5)));
+                this.progressBar.setProgress(position + 1);
+                break;
+            case 5:
+                this.content.setText(ctx.getResources().getString((R.string.tutorial_text6)));
+                this.progressBar.setProgress(position + 1);
+                break;
+            case 6:
+                this.content.setText(ctx.getResources().getString((R.string.tutorial_text7)));
+                this.progressBar.setProgress(position + 1);
+                break;
+            case 7:
+                this.imageView.setImageResource(R.drawable.check);
+                this.content.setText(ctx.getResources().getString((R.string.tutorial_text8)));
+                this.progressBar.setBackgroundColor(Color.WHITE);
+                this.progressBarBackground.setBackgroundColor(Color.WHITE);
+                break;
         }
 
+        container.addView(itemView);
+
         return itemView;
-    }
+}
 
 
     @Override
@@ -114,7 +106,7 @@ public class C_Adapter_Tutorial extends PagerAdapter {
     @Override
     public boolean isViewFromObject(View view, Object object) {
 
-        return  (view==object);
+        return (view == object);
     }
 }
 
