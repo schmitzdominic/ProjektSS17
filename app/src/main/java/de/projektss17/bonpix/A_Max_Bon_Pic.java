477752bd5d949bc5715108dd1575369c17104efd
Uli <uli.satzger@freenet.de>
2017-06-17 13:22:08 +0200

    - Klasse A_Max_Bon_Pic angepasst

--- app/src/main/java/de/projektss17/bonpix/A_Max_Bon_Pic.java
+++ app/src/main/java/de/projektss17/bonpix/A_Max_Bon_Pic.java
@@ -2,12 +2,11 @@ package de.projektss17.bonpix;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ImageView;
+
+
 
 public class A_Max_Bon_Pic extends AppCompatActivity {
 
@@ -25,5 +24,4 @@ public class A_Max_Bon_Pic extends AppCompatActivity {
         image.setImageURI(null);
         image.setImageURI(imageUri);
     }
-
 }
